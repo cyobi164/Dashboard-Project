c226bec500031f21ad37d9fe5034d05acbf290de
cyobi164 <mrgnchl090@gmail.com>
2026-06-25 00:16:25 +0900

    receipt 2

--- lib/pages/receipt_page.dart
+++ lib/pages/receipt_page.dart
@@ -29,13 +29,10 @@ class ReceiptPage extends StatelessWidget {
         receiptNo: "11673",
       ),
 
-      Receipt(
-        shop: "GU",
-        amount: 3000,
-        date: "Jan 12",
-        receiptNo: "12884",
-      ),
+      Receipt(shop: "GU", amount: 3000, date: "Jan 12", receiptNo: "12884"),
     ];
+
+    final total = recepits.fold(0.0, (sum, recepits) => sum + recepits.amount);
 
     return Scaffold(
       backgroundColor: const Color(0xff030617),
@@ -45,36 +42,59 @@ class ReceiptPage extends StatelessWidget {
         title: const Text("Receipts", style: TextStyle(color: Colors.white)),
       ),
 
-      body: ListView.builder(
-        padding: const EdgeInsets.all(20),
-        itemCount: recepits.length,
-
-        itemBuilder: (context, index) {
-          final r = recepits[index];
-
-          return Card(
-            color: const Color(0xFF1E293B),
-
-            child: ListTile(
-              leading: const Icon(Icons.receipt_long, color: Colors.orange),
-
-              title: Text(r.shop, style: const TextStyle(color: Colors.white)),
-
-              subtitle: Text(
-                "Receipt #${r.receiptNo} • ${r.date}",
-                style: const TextStyle(color: Colors.grey),
-              ),
-
-              trailing: Text(
-                "¥${NumberFormat('#,###').format(r.amount)}",
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: Text(
+              "Total: ¥${NumberFormat('#,###').format(total)}",
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
               ),
             ),
-          );
-        },
+          ),
+
+          Expanded(
+            child: ListView.builder(
+              padding: const EdgeInsets.all(20),
+              itemCount: recepits.length,
+              itemBuilder: (context, index) {
+                final r = recepits[index];
+
+                return Card(
+                  color: const Color(0xFF1E293B),
+
+                  child: ListTile(
+                    leading: const Icon(
+                      Icons.receipt_long,
+                      color: Colors.orange,
+                    ),
+
+                    title: Text(
+                      r.shop,
+                      style: const TextStyle(color: Colors.white),
+                    ),
+
+                    subtitle: Text(
+                      "Receipt #${r.receiptNo} • ${r.date}",
+                      style: const TextStyle(color: Colors.grey),
+                    ),
+
+                    trailing: Text(
+                      "¥${NumberFormat('#,###').format(r.amount)}",
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
