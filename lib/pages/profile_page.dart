37e18922a8ed7da56b6d116d4d8c4189a343c8de
cyobi164 <mrgnchl090@gmail.com>
2026-06-25 16:31:12 +0900

    Added user profilr

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -9,7 +9,88 @@ class ProfilePage extends StatelessWidget {
       backgroundColor: const Color(0xFF020617),
 
       appBar: AppBar(
+        backgroundColor: const Color(0xFF020617),
+        title: const Text("Profile", style: TextStyle(color: Colors.white)),
+      ),
 
+      body: ListView(
+        padding: const EdgeInsets.all(20),
+
+        children: [
+          const CircleAvatar(
+            radius: 50,
+            backgroundColor: Color(0xFF1E293B),
+            child: Icon(Icons.person, size: 50, color: Colors.white),
+          ),
+
+          const Center(
+            child: Text(
+              "User",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+
+          const Center(
+            child: Text(
+              "User@example.com",
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+
+          const SizedBox(height: 30),
+
+          Card(
+            color: const Color(0xFF1E293B),
+            child: Column(
+              children: const [
+                ListTile(
+                  leading: Icon(Icons.settings, color: Colors.white),
+                  title: Text("Setting", style: TextStyle(color: Colors.white)),
+                ),
+
+                Divider(height: 1),
+
+                ListTile(
+                  leading: Icon(Icons.notifications, color: Colors.white),
+                  title: Text(
+                    "Notifications",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+
+                Divider(height: 1),
+
+                ListTile(
+                  leading: Icon(Icons.lock, color: Colors.white),
+                  title: Text("Privacy", style: TextStyle(color: Colors.white)),
+                ),
+
+                Divider(height: 1),
+
+                ListTile(
+                  leading: Icon(Icons.help, color: Colors.white),
+                  title: Text(
+                    "Help & Support",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          const SizedBox(height: 20),
+          Card(
+            color: const Color(0XFF1E293B),
+            child: const ListTile(
+              leading: Icon(Icons.logout, color: Colors.white),
+              title: Text("Logout", style: TextStyle(color: Colors.white)),
+            ),
+          ),
+        ],
       ),
     );
   }
