7365ea0e205f303a846cadd34602bf628458bbef
cyobi164 <mrgnchl090@gmail.com>
2026-07-31 01:40:18 +0900

    updated animation

--- lib/features/auth/card_flip.dart
+++ lib/features/auth/card_flip.dart
@@ -4,6 +4,7 @@ import "package:app/features/auth/register_pages.dart";
 
 class CardFlip extends StatefulWidget {
   final VoidCallback onLogin;
+
   const CardFlip({super.key, required this.onLogin});
 
   @override
@@ -13,17 +14,49 @@ class CardFlip extends StatefulWidget {
 class _CardFlipState extends State<CardFlip>
     with SingleTickerProviderStateMixin {
   bool showLogin = true;
+
   late AnimationController _controller;
   late Animation<double> _animation;
 
   @override
   void initState() {
     super.initState();
+
     _controller = AnimationController(
       duration: const Duration(milliseconds: 500),
       vsync: this,
     );
-    _animation = Tween<double>(begin: 0, end: 3.14159).animate(_controller);
+
+    _animation = Tween<double>(
+      begin: 0,
+      end: 3.14159,
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeInOut));
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  Future<void> _flipCard() async {
+    if (_controller.isAnimating) return;
+
+    //90 degrees rotation
+    await _controller.animateTo(
+      0.5,
+      duration: const Duration(milliseconds: 250),
+    );
+
+    //change page here
+    setState(() {
+      showLogin = !showLogin;
+    });
+
+    //180 degrees rotation
+    await _controller.forward();
+    
+    _controller.reset();
   }
 
   @override
@@ -31,32 +64,51 @@ class _CardFlipState extends State<CardFlip>
     return Scaffold(
       backgroundColor: const Color(0xFF0F172A),
       body: Center(
-        child: Transform(
-          alignment: Alignment.center,
-          transform: Matrix4.identity()
-            ..setEntry(3, 2, 0.001)
-            ..rotateY(_animation.value),
-           /// child: child,
-            
-          child: showLogin
-              ? LoginPages(
-                  key: const ValueKey("login"),
-                  onLogin: widget.onLogin,
-                  onRegister: () {
-                    setState(() {
-                      showLogin = false;
-                    });
-                  },
-                )
-              : RegisterPages(
-                  key: const ValueKey("register"),
-                  onBackToLogin: () {
-                    setState(() {
-                      showLogin = true;
-                    });
-                  },
-                ),
-                
+        child: AnimatedBuilder(
+          animation: _animation,
+          builder: (context, child) {
+            final angle = _animation.value;
+
+            final isFront = angle <= 1.5708; // pi/2
+
+            return Transform(
+              alignment: Alignment.center,
+              transform: Matrix4.identity()
+                ..setEntry(3, 2, 0.001)
+                ..rotateY(_animation.value),
+              child: Stack(
+                alignment: Alignment.center,
+                children: [
+                  //front
+                  if (showLogin && isFront)
+                    LoginPages(onLogin: widget.onLogin, onRegister: _flipCard),
+
+                  // Register is back
+                  if (showLogin && !isFront)
+                    Transform(
+                      alignment: Alignment.center,
+                      transform: Matrix4.identity()..rotateY(3.14159),
+                      child: RegisterPages(onBackToLogin: _flipCard),
+                    ),
+
+                  //register
+                  if (!showLogin && isFront)
+                    RegisterPages(onBackToLogin: _flipCard),
+
+                  // Login is back
+                  if (!showLogin && !isFront)
+                    Transform(
+                      alignment: Alignment.center,
+                      transform: Matrix4.identity()..rotateY(3.14159),
+                      child: LoginPages(
+                        onLogin: widget.onLogin,
+                        onRegister: _flipCard,
+                      ),
+                    ),
+                ],
+              ),
+            );
+          },
         ),
       ),
     );
